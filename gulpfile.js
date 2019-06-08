7af9790da376fcd594241272c5b27d4e523baaf4
Jorge Moreno <alterebro@gmail.com>
2019-06-08 19:28:45 +0200

    Switched to SASS and extra classes

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 // const gulp = require('gulp');
 const { src, dest, parallel, watch } = require('gulp');
+const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 
 
 function postCSS() {
-    return src('./src/*.css')
+    return src('./src/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss([
-            require('postcss-nested'),
             require('autoprefixer'),
         ]))
         .pipe(dest('./dist'));
 }
 function postCSSmin() {
     return src('./src/*.css')
+    .pipe(sass().on('error', sass.logError))
         .pipe(postcss([
-            require('postcss-nested'),
             require('autoprefixer'),
             require('cssnano')
         ]))
@@ -25,5 +26,5 @@ function postCSSmin() {
 
 exports.build = parallel(postCSS, postCSSmin);
 exports.watch = function() {
-    watch('./src/*.css', parallel(postCSS, postCSSmin));
+    watch('./src/*.scss', parallel(postCSS, postCSSmin));
 }
